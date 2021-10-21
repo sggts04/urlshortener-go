e11ea864c53c81b75cdb58aa55f924dfa420844d
Shreyas Gupta <technology.shreyas@gmail.com>
2021-10-21 13:05:01 +0530

    use better if err syntax

--- main.go
+++ main.go
@@ -12,8 +12,7 @@ import (
 
 func main() {
 	// Load ENV Vars
-	err := godotenv.Load()
-	if err != nil {
+	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
 	PORT := os.Getenv("PORT")
